22e50fa601c8a54f43a3bc49a467ef32390dbee5
Kristena Arushanov <kris.arushanov@gmail.com>
2020-12-05 15:01:23 -0800

    updated api response

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
+/* Render to HTML on listWeather div */
+/*var listWeatherIcon = $(".listWeatherIcon"); */
+var listMainTemp = $(".listMainTemp");    
+var listWeatherMain = $(".listWeatherMain");
+var listWeatherDescription =$(".listWeatherDescription");
+
+/* Code written inside method will run once page DOM is ready to execute */
+
 $(document).ready(function(){
 
     /* Add click event to button using button ID */
@@ -7,7 +15,9 @@ $(document).ready(function(){
 
     /* Getting input value using input ID and val method */
         
-        var city = $("#yourCity").val();
+     var city = $("#yourCity").val();
+
+
       
 
     /* Use AJAX method to retrieve data*/
@@ -19,27 +29,36 @@ $(document).ready(function(){
             url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city +  "&units=imperial" + 
             "&APPID=63d898fccb40c6189c8cd5093662abf9",
 
-            method: "GET"
+            method: "GET",
+
     
           }).then (function(response){
-
-                var widget = show(response);
-                $("#show").html(widget);
-
-                console.log(response);
-                console.log(response.list[0].weather)
               
 
-                let results = response.list[0].weather;
-                console.log(results)
+
+                console.log(response);
+                /*listWeatherIcon.attr ("src", "https://openweathermap.org/img/w/" + response.list[0].weather[0].icon +".png");  */
+                listMainTemp.text("Temp:" + response.list[0].main.temp + "F");
+                listWeatherMain.text(response.list[0].weather[0].main);
+                listWeatherDescription.text("Description:" + response.list[0].weather[0].description);
+
+                var iconcode = response.list[0].weather[0].icon;
+                
+                
+                $(".listWeatherIcon").html("<img src='http://openweathermap.org/img/w/" + iconcode + ".png' alt='Icon depicting current weather.'>");
+                
+                
+                
+                /*$(".listWeatherIcon").attr('src', iconurl);*/
+
+                
             
          });   
+
+         
        
         });
+
 });
 
 
-function show(response){
-    return `<h2>${response.list[0].weather}</h2>`;
-}
-    
